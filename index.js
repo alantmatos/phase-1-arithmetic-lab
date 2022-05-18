a0a4ec69d2645eb42d0e201d294bffa542d65ac2
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-17 21:55:03 -0400

    commit tree def005a9e49be9f8af6360de87b87d4a4e69e455

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// test 1
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+// test 2
+let random = Math.floor(Math.random() * 10) + 1;
+
+// test 3
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// test 4
+const max = Math.max(1,2,4,15,20)
